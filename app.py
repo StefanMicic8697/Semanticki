25ee985f00b94a128fd69d30ed18e76f41c43d1c
cinc98 <ivan.cincurak98@gmail.com>
2020-11-16 13:07:01 +0100

    initialized graph

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from rdflib import Graph
 
 app = Flask(__name__)
 
+g = Graph()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
